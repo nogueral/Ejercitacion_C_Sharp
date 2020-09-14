ea483f86b7b32547d6a178721e73580bf997e69a
Leandro Noguera <nogueraleandro@hotmail.com>
2020-09-13 21:19:34 -0300

    Ej 20

---
+++ Ejercicio Nro 20/Ejercicio Nro 20/Peso.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Nro_20
+{
+#pragma warning disable CS0660
+#pragma warning disable CS0661
+
+    class Peso
+    {
+        double cantidad;
+        static double cotizRespectoDolar;
+
+        private Peso()
+        {
+            cotizRespectoDolar = 66;
+        }
+
+        public Peso(double cantidad) : this()
+        {
+            this.cantidad = cantidad;
+        }
+
+        public Peso(double cantidad, double cotizacion) : this(cantidad)
+        {
+            cotizRespectoDolar = cotizacion;
+        }
+
+        public double GetCantidad()
+        {
+            return this.cantidad;
+        }
+
+        public static double GetCotizacion()
+        {
+            return cotizRespectoDolar;
+        }
+
+        public static explicit operator Dolar (Peso auxPeso)
+        {
+            double auxCantidad = auxPeso.cantidad / Peso.GetCotizacion();
+
+            return new Dolar(auxCantidad);
+        }
+
+        public static bool operator ==(Peso auxPeso, Dolar auxDolar)
+        {
+            bool retorno = false;
+
+            if (auxDolar.GetCantidad() == auxPeso.GetCantidad())
+            {
+                retorno = true;
+            }
+
+            return retorno;
+        }
+
+        public static bool operator !=(Peso auxPeso, Dolar auxDolar)
+        {
+            return !(auxDolar.GetCantidad() == auxPeso.GetCantidad());
+        }
+
+        public static Peso operator +(Peso auxPeso, Dolar auxDolar)
+        {
+            Peso aux = new Peso();
+            double auxCantidad;
+
+            aux = (Peso)auxDolar;
+
+            auxCantidad = aux.GetCantidad() + auxPeso.GetCantidad();
+
+            return new Peso(auxCantidad);
+        }
+
+        public static Peso operator -(Peso auxPeso, Dolar auxDolar)
+        {
+            Peso aux = new Peso();
+            double auxCantidad;
+
+            aux = (Peso)auxDolar;
+
+            auxCantidad = aux.GetCantidad() - auxPeso.GetCantidad();
+
+            return new Peso(auxCantidad);
+        }
+    }
+}
---
+++ Ejercicio Nro 20/Ejercicio Nro 20/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_Nro_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dolar auxDolar = new Dolar(30);
+            Peso auxPeso = new Peso(2560);
+
+            Console.Title = "Ejercicio Nro 20";
+
+            
+            if (auxDolar == auxPeso)
+            {
+                Console.WriteLine("Son iguales");
+            }
+
+            if (auxPeso != auxDolar)
+            {
+                Console.WriteLine("Son distintos");
+            }
+
+            Dolar auxDolar2 = auxDolar + auxPeso;
+
+            Console.WriteLine("Cantidad dolares: {0:#,###.00}", auxDolar2.GetCantidad());
+
+            Peso auxPeso2 = auxPeso + auxDolar;
+
+            Console.WriteLine("Cantidad Pesos: {0:#,###.00}", auxPeso2.GetCantidad());
+        }
+    }
+}
